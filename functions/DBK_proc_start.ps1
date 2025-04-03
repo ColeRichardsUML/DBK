e049530ed9adef2b22ab8c6d34985bf59eb5a302
Diversified-Colman-Richards <colmanr@diversified-us.com>
2025-04-03 13:16:57 -0400

    rewrite

--- functions/DBK_proc_start.ps1
+++ functions/DBK_proc_start.ps1
@@ -6,7 +6,7 @@ function DBK_proc_start {
     DBK_dt_writer -wd "DBK_proc_start has been called."
 
     if (DBK_proc_is_running) {
-        DBK_dt_writer -wh ($config.process.exe + " is running.")
+        DBK_dt_writer -wh ($config.drive + ": is connected and " + $config.process.exe + " is running.")
     } else {
         DBK_dt_writer -wh ("Starting " + $config.process.exe + "...")
         Start-Process -FilePath $config.process.exe -WorkingDirectory $config.process.path
